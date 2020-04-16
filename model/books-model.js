f951623a72f946d5d3738dce392a449627867633
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-04-16 15:15:24 +0100

    Test auth middleware for deleting books Relates #18
    Co-authored-by: HettieM <hettie.mcconnell@outlook.com>

--- model/books-model.js
+++ model/books-model.js
@@ -1,11 +1,13 @@
 const db = require("../database/connection");
 
 function getAllBooks() {
-  return db.query("SELECT * FROM books;");
+  return db.query("SELECT * FROM books;").then((res) => res.rows);
 }
 
 function getBook(id) {
-  return db.query("SELECT * FROM books WHERE id = $1;", [id]);
+  return db
+    .query("SELECT * FROM books WHERE id = $1;", [id])
+    .then((res) => res.rows[0]);
 }
 
 function addBook(title, author, fiction) {
@@ -15,8 +17,8 @@ function addBook(title, author, fiction) {
   );
 }
 
-function deleteBook() {
-  // remove book from database
+function deleteBook(id) {
+  return db.query(`DELETE FROM books WHERE id=$1`, [id]);
 }
 
 function updateBook(id, update) {
--- model/users-model.js
+++ model/users-model.js
@@ -1,14 +1,21 @@
 const db = require("../database/connection");
 
 function getAllUsers() {
-  return db.query(`SELECT id, username, cohort FROM users;`)
-    .then(res => res.rows);
+  return db
+    .query(`SELECT id, username, cohort FROM users;`)
+    .then((res) => res.rows);
 }
 
 function getUser(username) {
   return db
     .query(`SELECT * FROM users WHERE username=$1;`, [username])
-    .then(res => res.rows[0]);
+    .then((res) => res.rows[0]);
+}
+
+function getUserById(id) {
+  return db
+    .query(`SELECT * FROM users WHERE id=$1;`, [id])
+    .then((res) => res.rows);
 }
 
 function addUser(username, password, cohort) {
@@ -17,7 +24,7 @@ function addUser(username, password, cohort) {
       `INSERT INTO users (username, password, cohort)VALUES($1, $2, $3);`,
       [username, password, cohort]
     )
-    .then(res => res.rows);
+    .then((res) => res.rows);
 }
 
-module.exports = { getAllUsers, addUser, getUser };
+module.exports = { getAllUsers, addUser, getUser, getUserById };
