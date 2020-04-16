aa898cd49db30fc467682b2e3c0a3fc7eeb0de20
Ina Yoon <itsina96@gmail.com>
2020-04-16 10:47:02 +0100

    Add middleware for authentication Relates #11
    Co-authored-by: Ivo-Evans <ievans.storrie@gmail.com>

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../model/users-model");
+const dotenv = require("dotenv");
+dotenv.config();
+const SECRET = process.env.JWT_SECRET;
+
+function verifyUser(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if ( !authHeader ) {
+    const error = new Error("Auth token required")
+    error.status = 404
+    next(error)
+  } else {
+    const token = authHeader.replace("Bearer ", "")
+    try {
+      const tokenData = jwt.verify(token, SECRET)
+      userModel
+        .getSpecificUser(tokenData.user)
+        .then((user) => {
+          req.user = user;
+          next()
+        })
+        .catch(next)
+    } catch (_err) {
+      const error = new Error('Not authorized')
+      error.status = 401
+      next(error)
+    }
+  }
+}
+
+module.exports = verifyUser;
